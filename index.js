cbcf2dd97251c95adcde3b888dec79d54e20cae0
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2025-08-20 15:00:52 +0700

    Update database

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE);
 
-app.get("/", (req, res) => {
+const Tour = mongoose.model(
+  "Tour",
+  {
+    name: String,
+    vehicle: String,
+  },
+  "tour"
+);
+
+app.get("/", async (req, res) => {
   res.send("Xin chào! Đây là trang chủ");
+  const tourList = await Tour.find({});
+  console.log(tourList);
 });
 
 app.listen(port, () => {
